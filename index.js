1e9acaeb4f01a5f73a4bebce881743c0d92ba835
Abigail Diaz <github email address>
2016-09-25 11:13:58 -0400

    Readme on Babel Plugins

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+
+// Rest properties
+var { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // { a: 3, b: 4 }
+
+// Spread properties
+var n = { x, y, ...z };
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
